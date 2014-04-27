1502d24c419574c417dcedc91023fa506f2784f0
TBaudon <baudon.thomas@gmail.com>
2014-04-27 21:15:41 +0200

    map

---
+++ Assets/data/levels/tiles.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="64" tileheight="64">
+ <image source="../../images/tilesets/cave1.png" width="512" height="1024"/>
+ <terraintypes>
+  <terrain name="crossable" tile="7"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,,"/>
+ <tile id="2" terrain=",0,,0"/>
+ <tile id="3" terrain=",,0,0"/>
+ <tile id="4" terrain="0,,0,"/>
+ <tile id="5" terrain="0,0,0,"/>
+ <tile id="6" terrain="0,0,,0"/>
+ <tile id="7" terrain=",0,0,0"/>
+ <tile id="8" terrain="0,,0,0"/>
+ <tile id="9" terrain="0,,,"/>
+ <tile id="10" terrain=",0,,"/>
+ <tile id="11" terrain=",,,0"/>
+ <tile id="12" terrain=",,0,"/>
+ <tile id="19" terrain="0,0,0,0"/>
+</tileset>
