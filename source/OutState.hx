aa80254cf6d38fa4586fbac54a5b2340bd0827b6
TBaudon <baudon.thomas@gmail.com>
2014-04-29 01:31:29 +0200

    end credits

--- source/OutState.hx
+++ source/OutState.hx
@@ -2,6 +2,7 @@ package ;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
+import flixel.FlxG;
 
 /**
  * ...
@@ -11,12 +12,15 @@ class OutState extends FlxState
 {
 
 	var theEnd : FlxText;
+	var thanks : FlxText;
 	
 	public function new() 
 	{
 		super();
 		
 		bgColor = 0xffffffff;
+		
+		trace("test");
 		
 		theEnd = new FlxText(0, 0, 0, "The End", 24);
 		theEnd.color = 0xff000000;
@@ -27,8 +31,28 @@ class OutState extends FlxState
 		FlxTween.tween(theEnd, { alpha:1 }, 1, { complete: endComplete } );
 	}
 	
-	function endComplete() {
-		
+	function endComplete(tween : FlxTween) {
+		thanks = new FlxText(0, 0, 0, "Thanks for playing", 24);
+		thanks.color = 0xff000000;
+		thanks.x = (800 - theEnd.width) / 2;
+		thanks.y = (480 - theEnd.height) / 2 + 20;
+		thanks.alpha = 0;
+		add(theEnd);
+		FlxTween.tween(thanks, { alpha:1 }, 1, { complete: thanksComplete } );
+	}
+	
+	function thanksComplete(tween : FlxTween) {
+		var tweet = new FlxText(0, 0, 0, "@damrem, @sentsu_actu, @ynck_33, @RadStar_", 24);
+		tweet.color = 0xff000000;
+		tweet.x = (800 - theEnd.width) / 2;
+		tweet.y = (480 - theEnd.height) / 2 + 40;
+		tweet.alpha = 0;
+		add(tweet);
+		FlxTween.tween(tweet, { alpha:1 }, 2, { complete: creditEnd } );
+	}
+	
+	function creditEnd(tween : FlxTween) {
+		FlxG.switchState(new MenuState());
 	}
 	
 }
