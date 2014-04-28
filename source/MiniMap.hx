df8060d628d4b46e53bc2b8d32d936ac561a4b13
TBaudon <baudon.thomas@gmail.com>
2014-04-28 13:04:10 +0200

    mini map fini

--- source/MiniMap.hx
+++ source/MiniMap.hx
@@ -24,11 +24,35 @@ class MiniMap extends FlxSprite
 		var mapX = 10 * Reg.levelTree.currentLevel.definition.long - 40;
 		var mapY = -10 * Reg.levelTree.currentLevel.definition.alt - 40;
 		
+		var roomLineStyle:LineStyle = { color: FlxColor.BLACK, thickness: 1 };
+		var roomFillStyle:FillStyle = { color: FlxColor.TRANSPARENT, alpha: 0.5 };
+		
 		for (def in Reg.levelTree.defs) {
-			if(def.explored)
-				FlxSpriteUtil.drawRect(this, def.long * 10 - mapX, -def.alt * 10 - mapY, 10, 10, 0x66cccccc);
-			if (def == Reg.levelTree.currentLevel.definition)
-				FlxSpriteUtil.drawRect(this, def.long * 10 - mapX, -def.alt * 10 - mapY, 10, 10, 0xcccccccc);
+			var drawDoors : Bool = false;
+			
+			var drawX = def.long * 10 - mapX;
+			var drawY = -def.alt * 10 - mapY;
+			
+			if(def.explored) {
+				FlxSpriteUtil.drawRect(this, drawX, drawY, 10, 10, 0x66cccccc, roomLineStyle, roomFillStyle);
+				drawDoors = true;
+			}
+			
+			if (def == Reg.levelTree.currentLevel.definition) {
+				FlxSpriteUtil.drawRect(this, drawX, drawY, 10, 10, 0xcccccccc, roomLineStyle, roomFillStyle);
+				drawDoors = true;
+			}
+			
+			if (drawDoors) {
+				if (def.leftMask == 1) 
+					FlxSpriteUtil.drawRect(this, drawX - 1, drawY + 3, 2, 4, 0xccffff00);
+				if (def.topMask == 1) 
+					FlxSpriteUtil.drawRect(this, drawX + 3, drawY - 1, 4, 2, 0xccffff00);
+				if (def.bottomMask == 1) 
+					FlxSpriteUtil.drawRect(this, drawX + 3, drawY + 9, 4, 2, 0xccffff00);
+				if (def.rightMask == 1) 
+					FlxSpriteUtil.drawRect(this, drawX + 9, drawY + 3, 2, 4, 0xccffff00);
+			}
 		}
 		
 		var lineStyle:LineStyle = { color: FlxColor.WHITE, thickness: 1 };
