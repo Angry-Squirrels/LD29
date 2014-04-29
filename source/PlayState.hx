f960220f992da2a396e2f20b7f9124c988c7a76b
TBaudon <baudon.thomas@gmail.com>
2014-04-29 04:06:33 +0200

    reset

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -220,6 +220,8 @@ class PlayState extends FlxState
 		
 		if(introDoor != null)
 			FlxG.collide(introDoor, hero.hitbox, introDoorCollide);
+			
+		if (FlxG.keys.justPressed.R) FlxG.resetState();
 		
 		FlxG.overlap(level.doors, this.hero.hitbox, touchDoor);
 		FlxG.collide(enemies, enemies);
